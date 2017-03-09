bbea63a2fc290c835eee113375a6231a2e6f26f7
Unknown <paosalcedo@gmail.com>
2017-03-09 12:01:27 -0500

    Tweaked Duck Sound. Music gets softer

--- AudioPong/Assets/Scripts/MoveRacket.cs
+++ AudioPong/Assets/Scripts/MoveRacket.cs
@@ -5,8 +5,13 @@ public class MoveRacket : MonoBehaviour {
     public float speed = 30;
     public string axis = "Vertical";
 
-    void FixedUpdate () {
-        float v = Input.GetAxisRaw(axis);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    void FixedUpdate ()
+	{
+		float v = Input.GetAxisRaw (axis);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, v) * speed;
+
+		if (Input.GetAxisRaw (axis) != 0) {
+			Debug.Log("moving!");
+		}
     }
 }
