9dc251a6cd887a031de6cdece13f8e73404076c6
Unknown <paosalcedo@gmail.com>
2017-03-09 12:22:57 -0500

    Added Hosni's creature sound

--- AudioPong/Assets/Scripts/MoveRacket.cs
+++ AudioPong/Assets/Scripts/MoveRacket.cs
@@ -2,16 +2,23 @@
 using System.Collections;
 
 public class MoveRacket : MonoBehaviour {
-    public float speed = 30;
+
+	public AudioSource creatureSource;
+//	public AudioClip creatureClip;    
+	public float speed = 30;
     public string axis = "Vertical";
+
+	void Start(){
+		creatureSource = GetComponent<AudioSource>();
+	}
 
     void FixedUpdate ()
 	{
 		float v = Input.GetAxisRaw (axis);
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, v) * speed;
 
-		if (Input.GetAxisRaw (axis) != 0) {
-			Debug.Log("moving!");
+		if (Input.GetAxisRaw (axis) != 0 && !creatureSource.isPlaying) {
+			creatureSource.Play();
 		}
     }
 }
